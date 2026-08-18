2eda837c0f311d14624d83d61792ca4baf5573d9
chienlmao <cnngn427@gmail.com>
2026-08-18 22:18:37 +0700

    update

--- buoi1/bai2.cpp
+++ buoi1/bai2.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 #include<string>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
+char a[100];
+cin>>a;
+for(int i=0; i<strlen(a); i++){
+    a[i]=toupper(a[i]);
+}
+cout<<a<<endl;
 
+for (int i=0; i<strlen(a); i++){
+    a[i]=tolower(a[i]);
+}
+cout<<a<<endl;
 }
